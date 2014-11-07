9dd6f7b4bd5002ab17a9e3a0bf5ffca35d14dcc9
Q Studio <ray@qstudio.us>
2014-11-07 18:44:45 +0100

    first commit

---
+++ js/request-callback.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($) {
+
+	var callbackFormWidth = $('.callback-btn-settings').attr('data-formwidth');
+        
+	if(!callbackFormWidth) {
+            callbackFormWidth = '400px';
+	}
+
+	$('.inline-container').width(callbackFormWidth);
+
+	if($.isFunction($.colorbox)){
+		$(".callback-form-show").colorbox({
+			width: callbackFormWidth,
+                        className: 'callback'
+		});
+	}
+	jQuery(document).ready(function($) {
+		$('.callback-form-container').submit(function() {
+			var formInputs = $(this).find('.validate');
+			var errors = '';
+
+			$(formInputs).each(function() {
+				if($.trim(this.value) == '') {
+					fieldLabel = $(this).parent().find('span.label-text').html();
+					errors += '- ' + fieldLabel + '\n';
+				}
+			});
+
+			if(errors.length > 0) {
+				alert('The following information is missing:\n\n' + errors);
+				return false;
+			}
+			else {
+				$('.submit-button').val('Please wait...');
+				$('.submit-button').attr('disabled', 'disabled');
+				return true;
+			}
+		});
+	});
+});
